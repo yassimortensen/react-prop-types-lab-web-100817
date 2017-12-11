34c7211ea04d9857fd5475e0d991f783f10faa32
yassimortensen <yassimortensen@gmail.com>
2017-12-11 16:09:37 -0500

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,47 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render(){
+    return (
+      <div className="product">
+        <ul>
+          <li>Name: {this.props.name}</li>
+          <li>Producer: {this.props.producer}</li>
+          <li>hasWatermark: {this.props.hasWatermark}</li>
+          <li>Color: {this.props.color}</li>
+          <li>Weight: {this.props.weight}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName) => {
+    const weight = props[propName]
+
+    if (weight === undefined) {
+      return new Error('The weight prop is required.')
+    }
+
+    if (isNaN(weight)) {
+      return new Error('The weight prop needs to be a number.')
+    }
+
+    const range = (weight > 80) && (weight < 300)
+
+    if (!range){
+      return new Error('The weight prop needs to be between 90 and 300.')
+    }
+  }
+}
+export default Product;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 import Product from './components/Product';
+
 
 ReactDOM.render(
   <Product
